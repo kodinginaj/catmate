1b54d26081b3cf8fefad870774e4a4289ad5af9d
GoldyWy <goldywidiyanto@gmail.com>
2020-05-03 21:34:20 +0700

    tambah asset/login, ganti link page auth

--- application/views/template/auth/footer_auth.php
+++ application/views/template/auth/footer_auth.php
@@ -1,12 +1,12 @@
 <!--===============================================================================================-->	
-	<script src="<?= base_url('vendor/login/vendor/jquery/jquery-3.2.1.min.js'); ?>"></script>
+	<script src="<?= base_url('assets/login/vendor/jquery/jquery-3.2.1.min.js'); ?>"></script>
 <!--===============================================================================================-->
-	<script src="<?= base_url('vendor/login/vendor/bootstrap/js/popper.js'); ?>"></script>
-	<script src="<?= base_url('vendor/login/vendor/bootstrap/js/bootstrap.min.js'); ?>"></script>
+	<script src="<?= base_url('assets/login/vendor/bootstrap/js/popper.js'); ?>"></script>
+	<script src="<?= base_url('assets/login/vendor/bootstrap/js/bootstrap.min.js'); ?>"></script>
 <!--===============================================================================================-->
-	<script src="<?= base_url('vendor/login/vendor/select2/select2.min.js'); ?>"></script>
+	<script src="<?= base_url('assets/login/vendor/select2/select2.min.js'); ?>"></script>
 <!--===============================================================================================-->
-	<script src="<?= base_url('vendor/login/js/main.js'); ?>"></script>
+	<script src="<?= base_url('assets/login/js/main.js'); ?>"></script>
 
 </body>
 </html>
